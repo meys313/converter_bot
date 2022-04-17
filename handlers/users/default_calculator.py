676041774b4abc69a344e47204807103c6255066
Dmitry <doesis04@gmail.com>
2022-04-17 11:55:43 +0300

    Немного уменьшил код, обрабатывающий калькулятор на дефолт. клавиатуре, убрал ненужные повторения

--- handlers/users/default_calculator.py
+++ handlers/users/default_calculator.py
@@ -10,17 +10,6 @@ from keyboards.default.default_calculator import calculator
 from states.keyboard import StatesKeyboard
 from filters import MyFilter
 
-
-@dp.message_handler(Command('test'))
-async def test(message: types.Message):
-    await message.answer('Простой калькулятор', reply_markup=calculator)
-    message = await message.answer('this is text')
-    await sleep(3)
-    await message.edit_text(text='this is edit text')
-
-
-
-
 @dp.message_handler(Command('standart'))
 async def state_start(message: types.Message, state: FSMContext):
     await StatesKeyboard.number.set()
@@ -30,21 +19,21 @@ async def state_start(message: types.Message, state: FSMContext):
 
     @dp.message_handler(MyFilter(), state=StatesKeyboard.number, content_types=types.ContentTypes.TEXT)
     async def process_state(message: types.Message, state: FSMContext):
-        if not await state.get_data():
-            await state.update_data(my_list=[message.text, ])
-        else:
-            async with state.proxy() as data:
+        async with state.proxy() as data:
+            if not data.get('my_list'):
+                data.update(my_list=[message.text, ])
+            else:
                 data['my_list'].append(message.text)
-        data = await state.get_data('my_list')
-        await message_io.edit_text(text=f"Вывод: {''.join(data.get('my_list'))}")
-        await message.chat.delete_message(message_id=message.message_id)
+                data.update()
+
+            await message_io.edit_text(text=f"Вывод: {''.join(data.get('my_list'))}")
+            await message.chat.delete_message(message_id=message.message_id)
 
 
         @dp.message_handler(filters.Regexp(r"^[.=]$"), state=StatesKeyboard.number)
         async def result( message: types.message, state: FSMContext):
-
             async with state.proxy() as data:
-
+                print(data['my_list'])
                 await message_io.edit_text(text=f'Результат = {eval("".join(data.get("my_list")))}')
                 await message.chat.delete_message(message_id=message.message_id)
                 await state.reset_data()
