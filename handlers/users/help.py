a66ad1aa721518844213e8bea68f341aedee1b6f
Dmitry <doesis04@gmail.com>
2022-05-11 21:46:33 +0300

    Добавил конвертер валют в список комманд. Добавил список комманд в /help

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -7,10 +7,20 @@ from loader import dp
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = ("Список команд: ",
+            "/cancel - Отменить действие ",
             "/default_calculator - обычный калькулятор",
             "/inline_calculator - инлайн калькутяор",
-            "/cancel - отмена | На главную ",
-            "/help - Справка"
+            "/help - Справка",
+            "/discount - Рассчитать скидку",
+            "/difference_between_dates - Определить разницу между двумя датами",
+            "/detect_years - Определить возраст",
+            "/detect_years - Определить возраст и через сколько следующий день рождения",
+            "/scale_of_notation - Перевести из одной системы счисления в другую",
+            "/time - конвертер времени",
+            "/weight - конвертер единиц измерения массы",
+            "/length - конвертер единиц измерения длины",
+            "/temperature - конвертер единиц измерения температуры",
+            "/currency - конвертер валют"
             )
     
     await message.answer("\n".join(text))
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,12 +4,13 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
+            types.BotCommand("currency", "конвертер валют"),
             types.BotCommand("temperature", "конвертер единиц измерения температуры"),
             types.BotCommand("length", "конвертер единиц измерения длины"),
             types.BotCommand("weight", "конвертер единиц измерения массы"),
             types.BotCommand("time", "конвертер времени"),
             types.BotCommand("scale_of_notation", "Перевести из одной системы счисления в другую"),
-            types.BotCommand("data_converter", "Конвертер единиц измерения информации"),
+            types.BotCommand("detect_years", "Определить возраст"),
             types.BotCommand("detect_years", "Определить возраст"),
             types.BotCommand("difference_between_dates", "Определить разницу между двумя датами"),
             types.BotCommand("discount", "Рассчитать скидку"),
